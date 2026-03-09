1eff48d2e37dcbe34e7ff2196dec3f8baf17a48f
tesseract-two <118913639+tesseract-two@users.noreply.github.com>
2026-03-09 19:23:44 -0400

    Save dimension photo was taken in to map data

--- src/main/java/net/blouflin/photography/PhotographyUtil.java
+++ src/main/java/net/blouflin/photography/PhotographyUtil.java
@@ -4,25 +4,16 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.DataResult;
-import com.mojang.serialization.Dynamic;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.map.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.RegistryOps;
-import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.dimension.DimensionTypes;
 import org.slf4j.Logger;
 
 import java.util.List;
@@ -61,139 +52,53 @@ public class PhotographyUtil {
         this.locked = locked;
     }
 
-    public static MapState fromNbt(NbtCompound nbt/*, RegistryWrapper.WrapperLookup registries*/) {
-        //DataResult var10000 = DimensionType.worldFromDimensionNbt(new Dynamic(NbtOps.INSTANCE, nbt.get("dimension")));
-//        DataResult var10000 = DimensionType.REGISTRY_CODEC.decode(new Dynamic(NbtOps.INSTANCE, nbt.get("dimension")));
-//        Logger var10001 = LOGGER;
-//        Objects.requireNonNull(var10001);
-//        RegistryKeys.toDimensionKey((RegistryKey)var10000.resultOrPartial(var10001::error).orElseThrow(() -> new IllegalArgumentException("Invalid map dimension: " + String.valueOf(nbt.get("dimension"))));)
-//        RegistryKey<World> registryKey = (RegistryKey)var10000;
-//        RegistryKey<World> registryKey = (RegistryKey)var10000.resultOrPartial(var10001::error).orElseThrow(() -> new IllegalArgumentException("Invalid map dimension: " + String.valueOf(nbt.get("dimension"))));
-        String w = nbt.getString("dimension", String.valueOf(DimensionTypes.OVERWORLD));
-        int i = nbt.getInt("xCenter", 0);
-        int j = nbt.getInt("zCenter", 0);
-        byte b = (byte) MathHelper.clamp(nbt.getByte("scale", (byte) 3), 0, 4);
-//        boolean bl = !nbt.contains("trackingPosition", 1) || nbt.getBoolean("trackingPosition", false);
-        boolean bl = nbt.getBoolean("showDecorations", false);
-        boolean bl2 = nbt.getBoolean("unlimitedTracking", false);
-        boolean bl3 = nbt.getBoolean("locked", true);
+    public static MapState fromNbt(NbtCompound nbt) {
 
-        //MapState mapState = new MapState(i, j, b, bl, bl2, bl3, RegistryKey.of(RegistryKeys.WORLD, Identifier.of("minecraft", "overworld")));
+        // unused map data; the MapState.of method used below accept these
+//        int i = nbt.getInt("xCenter", 0);
+//        int j = nbt.getInt("zCenter", 0);
+//        boolean bl = nbt.getBoolean("showDecorations", false);
+//        boolean bl2 = nbt.getBoolean("unlimitedTracking", false);
 
-        MapState mapState = MapState.of(b, bl3, RegistryKey.of(RegistryKeys.WORLD, Identifier.of("minecraft", "overworld")));
+        // used map data; provided to MapState.of below
+        String dimension = nbt.getString("dimension", "minecraft:overworld");
+        byte scale = (byte) MathHelper.clamp(nbt.getByte("scale", (byte) 3), 0, 4);
+        boolean locked = nbt.getBoolean("locked", true);
 
+        MapState mapState = MapState.of(scale, locked, RegistryKey.of(RegistryKeys.WORLD, Identifier.of(dimension)));
+
+        // add color data to map after creating it
         byte[] dummyByte = new byte[0];
-        byte[] bs = nbt.getByteArray("colors").orElse(dummyByte);
-        if (bs.length == 16384) {
-            mapState.colors = bs;
+        byte[] colorBytes = nbt.getByteArray("colors").orElse(dummyByte);
+        if (colorBytes.length == 16384) {
+            mapState.colors = colorBytes;
         }
-
-//        RegistryOps<NbtElement> registryOps = registries.getOps(NbtOps.INSTANCE);
-
-//        for(MapBannerMarker mapBannerMarker : (List)MapBannerMarker.LIST_CODEC.parse(registryOps, nbt.get("banners")).resultOrPartial((banner) -> LOGGER.warn("Failed to parse map banner: '{}'", banner)).orElse(List.of())) {
-//            mapState.banners.put(mapBannerMarker.getKey(), mapBannerMarker);
-//            mapState.addDecoration(mapBannerMarker.getDecorationType(), (WorldAccess)null, mapBannerMarker.getKey(), (double)mapBannerMarker.pos().getX(), (double)mapBannerMarker.pos().getZ(), (double)180.0F, (Text)mapBannerMarker.name().orElse((Object)null));
-//        }
-
-//        NbtList nbtList = nbt.getList("frames", 10);
-
-//        for(int k = 0; k < nbtList.size(); ++k) {
-//            MapFrameMarker mapFrameMarker = MapFrameMarker.fromNbt(nbtList.getCompound(k));
-//            if (mapFrameMarker != null) {
-//                mapState.frames.put(mapFrameMarker.getKey(), mapFrameMarker);
-//                mapState.addDecoration(MapDecorationTypes.FRAME, (WorldAccess)null, getFrameDecorationKey(mapFrameMarker.getEntityId()), (double)mapFrameMarker.getPos().getX(), (double)mapFrameMarker.getPos().getZ(), (double)mapFrameMarker.getRotation(), (Text)null);
-//            }
-//        }
 
         return mapState;
     }
 
 
-    public static NbtCompound writeNbt(NbtCompound nbt, MapState state/*, RegistryWrapper.WrapperLookup registries*/) {
-        DataResult var10000 = Identifier.CODEC.encodeStart(NbtOps.INSTANCE, state.dimension.getValue());
+    public static NbtCompound writeNbt(NbtCompound nbt, MapState state) {
+
+        //unused data; see fromNbt method above
+//        nbt.putInt("xCenter", state.centerX);
+//        nbt.putInt("zCenter", state.centerZ);
+//
+//        nbt.putBoolean("showDecorations", state.showDecorations);
+//        nbt.putBoolean("unlimitedTracking", state.unlimitedTracking);
+
+
+        // get dimension
+        DataResult<NbtElement> var10000 = Identifier.CODEC.encodeStart(NbtOps.INSTANCE, state.dimension.getValue());
         Logger var10001 = LOGGER;
         Objects.requireNonNull(var10001);
-        //var10000.resultOrPartial(var10001::error).ifPresent((dimension) -> nbt.put("dimension", dimension));
-        //nbt.put("dimension", var10000.resultOrPartial().ifPresent((dimension) -> nbt.put("dimension", (NbtElement) dimension));
-        nbt.putInt("xCenter", state.centerX);
-        nbt.putInt("zCenter", state.centerZ);
-        nbt.putByte("scale", state.scale);
+        var10000.resultOrPartial(var10001::error).ifPresent((nbtElement) -> nbt.put("dimension", nbtElement));
+
+        // get colors, scale, and locked/unlocked status
         nbt.putByteArray("colors", state.colors);
-        nbt.putBoolean("trackingPosition", state.showDecorations);
-        nbt.putBoolean("unlimitedTracking", state.unlimitedTracking);
+        nbt.putByte("scale", state.scale);
         nbt.putBoolean("locked", state.locked);
-//        RegistryOps<NbtElement> registryOps = registries.getOps(NbtOps.INSTANCE);
-//        nbt.put("banners", (NbtElement)MapBannerMarker.LIST_CODEC.encodeStart(registryOps, List.copyOf(this.banners.values())).getOrThrow());
-//        NbtList nbtList = new NbtList();
 
-//        for(MapFrameMarker mapFrameMarker : this.frames.values()) {
-//            nbtList.add(mapFrameMarker.toNbt());
-//        }
-
-//        nbt.put("frames", nbtList);
         return nbt;
     }
-
-//    public static MapState fromNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
-//        DataResult var10000 = DimensionType.worldFromDimensionNbt(new Dynamic(NbtOps.INSTANCE, nbt.get("dimension")));
-//        DataResult var10000 = DimensionType.
-//        Logger var10001 = LOGGER;
-//        Objects.requireNonNull(var10001);
-//        RegistryKey<World> registryKey = (RegistryKey)var10000.resultOrPartial(var10001::error).orElseThrow(() -> new IllegalArgumentException("Invalid map dimension: " + String.valueOf(nbt.get("dimension"))));
-//        RegistryKey<World> registryKey =
-//        int i = nbt.getInt("xCenter");
-//        int j = nbt.getInt("zCenter");
-//        byte b = (byte)MathHelper.clamp(nbt.getByte("scale"), 0, 4);
-//        boolean bl = !nbt.contains("trackingPosition", 1) || nbt.getBoolean("trackingPosition");
-//        boolean bl2 = nbt.getBoolean("unlimitedTracking");
-//        boolean bl3 = nbt.getBoolean("locked");
-//        MapState mapState = new MapState(i, j, b, bl, bl2, bl3, registryKey);
-//        byte[] bs = nbt.getByteArray("colors");
-//        if (bs.length == 16384) {
-//            mapState.colors = bs;
-//        }
-//
-//        RegistryOps<NbtElement> registryOps = registryLookup.getOps(NbtOps.INSTANCE);
-//
-//        for(MapBannerMarker mapBannerMarker : (List)MapBannerMarker.LIST_CODEC.parse(registryOps, nbt.get("banners")).resultOrPartial((banner) -> LOGGER.warn("Failed to parse map banner: '{}'", banner)).orElse(List.of())) {
-//            mapState.banners.put(mapBannerMarker.getKey(), mapBannerMarker);
-//            mapState.addDecoration(mapBannerMarker.getDecorationType(), (WorldAccess)null, mapBannerMarker.getKey(), (double)mapBannerMarker.pos().getX(), (double)mapBannerMarker.pos().getZ(), (double)180.0F, (Text)mapBannerMarker.name().orElse((Object)null));
-//        }
-//
-//        NbtList nbtList = nbt.getList("frames", 10);
-//
-//        for(int k = 0; k < nbtList.size(); ++k) {
-//            MapFrameMarker mapFrameMarker = MapFrameMarker.fromNbt(nbtList.getCompound(k));
-//            if (mapFrameMarker != null) {
-//                mapState.frames.put(mapFrameMarker.getKey(), mapFrameMarker);
-//                mapState.addDecoration(MapDecorationTypes.FRAME, (WorldAccess)null, getFrameDecorationKey(mapFrameMarker.getEntityId()), (double)mapFrameMarker.getPos().getX(), (double)mapFrameMarker.getPos().getZ(), (double)mapFrameMarker.getRotation(), (Text)null);
-//            }
-//        }
-//
-//        return mapState;
-//    }
-//
-//    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
-//        DataResult var10000 = Identifier.CODEC.encodeStart(NbtOps.INSTANCE, this.dimension.getValue());
-//        Logger var10001 = LOGGER;
-//        Objects.requireNonNull(var10001);
-//        var10000.resultOrPartial(var10001::error).ifPresent((nbtElement) -> nbt.put("dimension", nbtElement));
-//        nbt.putInt("xCenter", this.centerX);
-//        nbt.putInt("zCenter", this.centerZ);
-//        nbt.putByte("scale", this.scale);
-//        nbt.putByteArray("colors", this.colors);
-//        nbt.putBoolean("trackingPosition", this.showDecorations);
-//        nbt.putBoolean("unlimitedTracking", this.unlimitedTracking);
-//        nbt.putBoolean("locked", this.locked);
-//        RegistryOps<NbtElement> registryOps = registryLookup.getOps(NbtOps.INSTANCE);
-//        nbt.put("banners", (NbtElement)MapBannerMarker.LIST_CODEC.encodeStart(registryOps, List.copyOf(this.banners.values())).getOrThrow());
-//        NbtList nbtList = new NbtList();
-//
-//        for(MapFrameMarker mapFrameMarker : this.frames.values()) {
-//            nbtList.add(mapFrameMarker.toNbt());
-//        }
-//
-//        nbt.put("frames", nbtList);
-//        return nbt;
-//    }
 }
